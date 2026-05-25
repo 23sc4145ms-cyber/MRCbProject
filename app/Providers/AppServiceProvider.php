1084937788b7cf00b50c8253866473ea002fa6da
Michelle <23sc4147_ms@psu.edu.ph>
2026-05-25 21:41:53 +0800

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 use App\Observers\UserObserver;
 
@@ -23,10 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
         Paginator::useBootstrapFive();
         
         // Register the UserObserver to automatically create a profile when a user is created
         User::observe(UserObserver::class);
+
+
     }
 }

9f0d2582ff4f93034033f29b73939dc11b6c5b47
Michelle <23sc4147_ms@psu.edu.ph>
2026-05-27 23:46:39 +0800

    fix teacher password route

--- resources/views/dashboards/teacher.blade.php
+++ resources/views/dashboards/teacher.blade.php
@@ -422,7 +422,7 @@
                 </div>
             @endif
 
-            <form action="{{ route('teachers.password.update') }}" method="POST" class="password-form" onsubmit="return confirmPasswordChange()">
+            <form action="{{ route('teacher.password.update') }}" method="POST" class="password-form" onsubmit="return confirmPasswordChange()">
                 @csrf
                 
                 <div class="form-group">
